2599249bb9eb7dad91293ac08e2bf11b3e3b7c8b
Francisco Encalada <encalada.francisco@outlook.com>
2024-09-23 19:21:02 -0500

    Initial commit

---
+++ src/main/java/com/jar/xsmartroutebackend/XSmartRouteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jar.xsmartroutebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XSmartRouteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XSmartRouteBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jar/xsmartroutebackend/XSmartRouteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jar.xsmartroutebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XSmartRouteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
